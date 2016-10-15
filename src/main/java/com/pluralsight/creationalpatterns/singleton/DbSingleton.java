42617d6b527f00655b8f2b6c1837075b63728335
Jerry Hu <hh201020@gmail.com>
2016-10-15 13:46:41 -0400

    Add Database Singleton as example

--- src/main/java/com/pluralsight/creationalpatterns/singleton/DbSingleton.java
+++ src/main/java/com/pluralsight/creationalpatterns/singleton/DbSingleton.java
@@ -1,15 +1,26 @@
 package com.pluralsight.creationalpatterns.singleton;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbSingleton {
 	
 	private static DbSingleton instance = null;
-	
+	private Connection conn = null;
+
 	private DbSingleton (){
+		try {
+			DriverManager.registerDriver(new org.apache.derby.jdbc.EmbeddedDriver());			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
+//	public static synchronized DbSingleton getInstance() { synchronized every time, slow it down.
 	public static DbSingleton getInstance() {
-		
-		if (instance == null) {
+
+		if (instance == null) {		// make sure to go to synchronized only the first time
 			synchronized (DbSingleton.class) {
 				if (instance == null) {
 					instance = new DbSingleton();
@@ -18,5 +29,23 @@ public class DbSingleton {
 		}
 		
 		return instance;
+	}
+	
+	public Connection getConnection() {
+		if(conn == null) {
+			synchronized (DbSingleton.class) {
+				if(conn == null) {
+					try {
+						String dbUrl = "jdbc:derby:memory:codejava/webdb;create=true";
+						
+						conn = DriverManager.getConnection(dbUrl);
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		
+		return conn;
 	}
 }
--- src/main/java/com/pluralsight/creationalpatterns/singleton/DbSingletonDemo.java
+++ src/main/java/com/pluralsight/creationalpatterns/singleton/DbSingletonDemo.java
@@ -1,14 +1,41 @@
 package com.pluralsight.creationalpatterns.singleton;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DbSingletonDemo {
 
 	public static void main(String[] args) {
+		long timeBefore = 0;
+		long timeAfter = 0;
 		
 		DbSingleton instance = DbSingleton.getInstance();
-		System.out.println(instance);
-		DbSingleton anotherInstance = DbSingleton.getInstance();
-		System.out.println(anotherInstance);
-				
-	}
+		
+		timeBefore = System.currentTimeMillis();
+		Connection conn = instance.getConnection();
+		timeAfter = System.currentTimeMillis();
 
+		System.out.println(timeAfter - timeBefore);
+
+		
+		Statement sta;
+		try {
+			sta = conn.createStatement();
+			int count = sta
+					.executeUpdate("CREATE TABLE Address (ID INT, StreetName VARCHAR(20),"
+							+ " City VARCHAR(20))");
+			System.out.println("Table created.");
+			sta.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		timeBefore = System.currentTimeMillis();	// take no time to get Singleton
+		conn = instance.getConnection();
+		timeAfter = System.currentTimeMillis();
+		
+		System.out.println(timeAfter - timeBefore);
+	}
 }
