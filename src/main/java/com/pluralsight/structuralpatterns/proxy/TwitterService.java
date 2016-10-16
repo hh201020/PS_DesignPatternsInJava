dbf6337367522a4ceab5dbc56a1a2954d080bf64
Jerry Hu <hh201020@gmail.com>
2016-10-16 14:41:40 -0400

    proxy pattern: stub example

---
+++ src/main/java/com/pluralsight/structuralpatterns/proxy/TwitterService.java
@@ -0,0 +1,6 @@
+package com.pluralsight.structuralpatterns.proxy;
+
+public interface TwitterService {
+	public String getTimeline(String screenName);
+	public void postToTimeline(String screenName, String message);
+}
